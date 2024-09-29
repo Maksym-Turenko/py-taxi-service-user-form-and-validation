fa6ee9b71d48285284744fe27deabada48ccdf03
Maxim Turenko <turenkomaksim099@gmail.com>
2024-09-29 16:12:04 +0300

    fix: flake8

--- taxi/urls.py
+++ taxi/urls.py
@@ -41,7 +41,11 @@ urlpatterns = [
         name="manufacturer-delete",
     ),
     path("cars/", CarListView.as_view(), name="car-list"),
-    path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),  # Логика добавления/удаления водителя теперь тут
+    path(
+        "cars/<int:pk>/",
+        CarDetailView.as_view(),
+        name="car-detail"
+    ),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
